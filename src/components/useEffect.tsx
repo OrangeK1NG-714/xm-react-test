01320c4f9250c2181d54d4130aad3c4bb423d038
邱琳超 <qiulinchao@supcon.com>
2026-01-04 14:21:48 +0800

    useEffect使用

--- src/components/useEffect.tsx
+++ src/components/useEffect.tsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react"
 const UseEffect=()=>{
+    const [count,setCount] =useState(0)
+    const [name,setName] = useState('')
+    useEffect(()=>{
+      console.log('执行了', count, name)
+      return ()=>{
+        console.log('卸载了');
+      }
+    },[count])
     return (
         <div>
-            UseEffect
-            
+            <div>
+                <h3>count:{count}</h3>
+                <button onClick={()=>{setCount(count+1)}}>+</button>
+            </div>         
+            <div>
+                <h3>name:{name}</h3>
+                <input value={name} onChange={(e)=>setName(e.target.value)} />    
+            </div>   
         </div>
     )
 }
